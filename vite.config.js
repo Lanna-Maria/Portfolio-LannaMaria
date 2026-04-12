c2caeb34920d893abf5e07d0ade1871c3e5f5d20
Lanna-Maria <lannamarya2016@gmail.com>
2026-04-12 00:08:06 -0300

    confif vite.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/portifolio-Lanna/',
+  base: '/Portfolio-LannaMaria/',
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
